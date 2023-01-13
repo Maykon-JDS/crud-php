875a0bddf28d1fde2ef3d38ab7eecb6c25ea18d0
Maykon Dias <Maykondias2001@gmail.com>
2023-01-12 23:47:10 -0300

    CRUD - Part 1

---
+++ crud/cadastrar.php
@@ -0,0 +1,11 @@
+<?php
+
+require(__DIR__ . '/vendor/autoload.php');
+
+if(isset($_POST['titulo'],$_POST['descricao'],$_POST['ativo'])){
+    die('Cadastrar');
+}
+
+include(__DIR__ . '/include/header.php');
+include(__DIR__ . '/include/formulario.php');
+include(__DIR__ . '/include/footer.php');
