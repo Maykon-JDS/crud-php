39c1b6b145c701be9a8f51c649584499bda3272e
Maykon Dias <Maykondias2001@gmail.com>
2023-01-16 18:07:38 -0300

    Estudo - Part 1

--- crud/include/header.php
+++ crud/include/header.php
@@ -1,12 +1,12 @@
-<!doctype html>
-<html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!doctype html> <!-- O <!doctype html> é uma declaração para informar qual a verção do HTML está sendo utilizado no arquivo, neste caso está sendo representato o HTML 5, a versão mais nova do HTML. -->
+<html lang="pt-br"> <!-- O atributo 'lang' define para o navegador a linguagem de determinado bloco HTML, no exemplo, especifica que o conteudo inteiro dentro da tag html esta em portugues -->
+  <head> <!-- A tag <head> providencia informações gerais (metadados) sobre o documento, incluindo seu titulo e links para scripts e folhas de estilos -->
+    <meta charset="utf-8"> <!-- O atributo 'charset' especifica qual a codificação de caracteres utilizada na página -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <!-- a tag meta com o atributo 'viewport' junto ao atributo 'content' define o tamanho da janela de visualização da página -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <title>Vagas</title>
   </head>
-  <body class="bg-dark text-light">
+  <body class="bg-dark text-light"> 
 <div class="container">
 
 <div class="jumbotron bg-danger">
--- crud/index.php
+++ crud/index.php
@@ -1,12 +1,12 @@
 <?php
 
-require(__DIR__ . '/vendor/autoload.php');
+require(__DIR__ . '/vendor/autoload.php'); //Utilizado para incluir um arquivo expecificado, porem se houver um erro irá esté será fatal, parando a execução do programa.
 
-use \App\Entity\Vaga;
+use \App\Entity\Vaga; //Utilizado para chamar uma determinada namespace
 
-$vagas = Vaga::getVagas();
+$vagas = Vaga::getVagas(); //Atribuindo valor retornando da função estatica 'Vaga::getVagas()' a variavel '$vagas'. Um atributo ou procedimento/função estatica pode ser chamada sem precisar instanciar a classe que ela está contida.
  
-
-include(__DIR__ . '/include/header.php');
+ 
+include(__DIR__ . '/include/header.php'); //Inclui um arquivo expecificado
 include(__DIR__ . '/include/listagem.php');
 include(__DIR__ . '/include/footer.php');
